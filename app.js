1b7e7251be445e662e04664514ca63b12210c890
Coni <coti.garibotto@gmail.com>
2023-10-25 16:06:01 -0300

    update form

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const images = document.querySelectorAll('.carousel img');
 
 let ImageIndex = 0;
 let automaticSlideInterval; // variable para hacerlo automatico 
-
+ 
 function showImage(index) {
     images.forEach((image, i) => {
         if (i === index) {
@@ -46,48 +46,89 @@ showImage(ImageIndex);
 startAutomaticSlide();
 
 /*FORRRRRRRRRRMULARIO*/
+
 const fullName = document.getElementById('fullName');
 const email = document.getElementById('email');
 const phoneNumber = document.getElementById('phoneNumber');
 const comments = document.getElementById('comments');
 const form = document.getElementById('myForm');
 const errorElement = document.getElementById('error');
+const confirmationMessage = document.getElementById('confirmationMessage');
 
 form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
     let messages = [];
-  
+
     if (fullName.value.trim() === '') {
         messages.push('Full Name is required');
         fullName.classList.add('error');
     } else {
         fullName.classList.remove('error');
     }
-  
+
     if (email.value.trim() === '' || !/^\S+@\S+\.\S+$/.test(email.value)) {
         messages.push('Please enter a valid Email');
         email.classList.add('error');
     } else {
         email.classList.remove('error');
     }
-  
+
     if (phoneNumber.value.trim() === '') {
         messages.push('Phone Number is required');
         phoneNumber.classList.add('error');
     } else {
         phoneNumber.classList.remove('error');
     }
-  
+
     if (comments.value.trim() === '') {
         messages.push('Comments are required');
         comments.classList.add('error');
     } else {
         comments.classList.remove('error');
     }
-  
+
     if (messages.length > 0) {
-        e.preventDefault();
         errorElement.innerHTML = messages.join('<br>');
+        confirmationMessage.style.display = 'none';
     } else {
+        // Si no hay errores, muestra los datos enviados
         errorElement.innerHTML = '';
+        displaySubmittedData();
     }
 });
+
+function displaySubmittedData() {
+    // Obtener los valores de los campos
+    const submittedFullName = fullName.value;
+    const submittedEmail = email.value;
+    const submittedPhoneNumber = phoneNumber.value;
+    const submittedComments = comments.value;
+
+    // Crear elementos HTML para mostrar los datos
+    const fullNameElement = document.createElement('p');
+    fullNameElement.textContent = `Full Name: ${submittedFullName}`;
+
+    const emailElement = document.createElement('p');
+    emailElement.textContent = `Email: ${submittedEmail}`;
+
+    const phoneNumberElement = document.createElement('p');
+    phoneNumberElement.textContent = `Phone Number: ${submittedPhoneNumber}`;
+
+    const commentsElement = document.createElement('p');
+    commentsElement.textContent = `Comments: ${submittedComments}`;
+
+    // Agregar los elementos al div de confirmación
+    confirmationMessage.innerHTML = '';
+    confirmationMessage.appendChild(fullNameElement);
+    confirmationMessage.appendChild(emailElement);
+    confirmationMessage.appendChild(phoneNumberElement);
+    confirmationMessage.appendChild(commentsElement);
+
+    confirmationMessage.style.display = 'block';
+}
+
+
+
+
+
